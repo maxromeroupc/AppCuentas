35582c69f680c46daa9de1d4e5368fb020654100
maxromeroupc <maxrichard.1989@gmail.com>
2020-03-11 10:32:41 -0500

    subiendo el proyecto.

---
+++ app/src/main/java/com/example/appcuentas/Entidades/Ventas.java
@@ -0,0 +1,60 @@
+package com.example.appcuentas.Entidades;
+
+public class Ventas {
+
+    private int idVentas;
+    private int idProducto;
+    private String producto;
+    private int cantidad;
+    private float precio;
+    private float costo;
+
+
+    public int getIdVentas() {
+        return idVentas;
+    }
+
+    public void setIdVentas(int idVentas) {
+        this.idVentas = idVentas;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+}
